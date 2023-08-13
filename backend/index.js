5831094bbe535d50c67b9de6548332204cf29b20
Admin <iheachochristian@yahoo.com>
2023-08-13 22:31:10 +0100

    Implemented logic to handle user authentication

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,14 @@ const app = express()
 const port = 3000
 const productRouter = require('./routes/ProductsRoute')
 const authRouter = require("./routes/AuthRoute")
+const userRoute = require('./routes/UserRoute')
 
 const dotenv = require("dotenv")
 const mongoose = require('mongoose')
 
 dotenv.config()
 
+//DB Connection
 mongoose.connect(process.env.MONGOOSE_DB_URL).then(() => {
     console.log("Connected to MongoDB")
 })
@@ -17,7 +19,9 @@ mongoose.connect(process.env.MONGOOSE_DB_URL).then(() => {
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
 
+//Endpoints
 app.use('/api/product', productRouter)
 app.use('/api/', authRouter)
+app.use('/api', userRoute)
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT}!`)) 
